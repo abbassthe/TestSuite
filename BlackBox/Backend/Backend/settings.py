dbc763956d121cea3842db8dd79e715db0bce807
abbass <abbassnaim2005@gmail.com>
2024-08-25 11:18:43 +0300

    cors

--- BlackBox/Backend/Backend/settings.py
+++ BlackBox/Backend/Backend/settings.py
@@ -37,10 +37,12 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "API"
+    "API",
+    'corsheaders',
 ]
-
+CORS_ALLOW_ALL_ORIGINS = True
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
